cb915e088e4ab03682cf84bd69b9ece52d5c7f0b
Cash <cashleo@outlook.com>
2019-10-02 20:26:42 -0700

    First commit Bank Project

---
+++ BankAccount.java
@@ -0,0 +1,40 @@
+package BankInfo;
+import java.lang.*;
+
+public class BankAccount {
+
+	
+	private
+	
+		String account = "123456789";
+		double balance = 50.0;
+		String name = "Chong Liu";
+		String email = "cashleo@outlook.com";
+		String phone = "4256330972";
+	
+
+	public
+		void deposit(double amount) {
+			balance = balance + amount;
+		}
+		void withdraw (double amount){
+			if(amount > balance) {
+				System.out.println("Customer"+ name);
+				System.out.println("You have insufficient balance, the max you can withdraw is: " + balance );
+			}
+			else {
+				balance = balance - amount;
+			}
+		}
+		void display_info () {
+			System.out.println(name);
+			System.out.println(account);
+			System.out.println(balance);
+			System.out.println(email);
+			System.out.println(phone);
+				
+		}
+		
+
+}
+
---
+++ UserTest.java
@@ -0,0 +1,11 @@
+package BankInfo;
+import BankInfo.BankAccount;
+
+public class UserTest {
+	
+	
+	public static void main(String agr[]) {
+		BankAccount user1 = new BankAccount();
+		user1.display_info();
+	}
+}
